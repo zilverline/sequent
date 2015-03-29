0c33f6c708aa0b2dc03b1ebe8ce3752602af4235
Lars Vonk <lars.vonk@gmail.com>
2015-03-29 13:30:25 +0200

    Remove method that does not belong in sequent

--- lib/sequent/core/tenant_event_store.rb
+++ lib/sequent/core/tenant_event_store.rb
@@ -5,11 +5,6 @@ module Sequent
   module Core
     class TenantEventStore < EventStore
 
-      ## TODO remove this method since created_organizations is Freemle specific
-      def self.all_organization_ids(record_class = Sequent::Core::EventRecord)
-        record_class.created_organizations.pluck(:aggregate_id)
-      end
-
       def replay_events_for(organization_id)
         replay_events do
           aggregate_ids = @record_class.connection.select_all("select distinct aggregate_id from #{@record_class.table_name} where organization_id = '#{organization_id}'").map { |hash| hash["aggregate_id"] }
