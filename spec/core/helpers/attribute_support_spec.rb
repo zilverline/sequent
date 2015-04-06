684fbfa28b2051a0656d6eb190f95f8fe471b88b
Lars Vonk <lars.vonk@gmail.com>
2015-04-06 15:26:36 +0200

    More readable spec

--- spec/core/helpers/attribute_support_spec.rb
+++ spec/core/helpers/attribute_support_spec.rb
@@ -1,75 +1,79 @@
 require 'spec_helper'
 
 describe Sequent::Core::Helpers::AttributeSupport do
+  context ".validation_errors" do
+    class NestedTestClass
+      include Sequent::Core::Helpers::AttributeSupport, ActiveModel::Validations
+      attrs message: String
+      validates_presence_of :message
+    end
 
-  class NestedTestClass
-    include Sequent::Core::Helpers::AttributeSupport, ActiveModel::Validations
-    attrs message: String
-    validates_presence_of :message
+    class AttributeSupportTestClass
+      include Sequent::Core::Helpers::AttributeSupport, ActiveModel::Validations
+      attrs message: String, nested_test_class: NestedTestClass
+      validates_presence_of :message
+      validates_with Sequent::Core::Helpers::AssociationValidator, associations: :nested_test_class
+    end
+
+    class SubTestClass < NestedTestClass
+      attrs sub_message: String
+      validates_presence_of :sub_message
+    end
+
+    it "returns validation errors as hash" do
+      subject = NestedTestClass.new
+      expect(subject.valid?).to be_falsey
+      expect(subject.validation_errors).to have(1).items
+      expect(subject.validation_errors[:message]).to_not be_nil
+    end
+
+    it "returns all validations of nested classes in same hash" do
+      subject = AttributeSupportTestClass.new
+      subject.nested_test_class = NestedTestClass.new
+      expect(subject.valid?).to be_falsey
+      expect(subject.validation_errors).to have(3).items
+      expect(subject.validation_errors[:message]).to_not be_nil
+      expect(subject.validation_errors[:nested_test_class]).to_not be_nil
+      expect(subject.validation_errors[:nested_test_class_message]).to_not be_nil
+    end
+
+    it "returns all validation errors including from superclass" do
+      subject = SubTestClass.new
+      expect(subject.valid?).to be_falsey
+      expect(subject.validation_errors).to have(2).items
+      expect(subject.validation_errors[:message]).to_not be_nil
+      expect(subject.validation_errors[:sub_message]).to_not be_nil
+    end
+
+    it "does not fail when it does not have errors" do
+      subject = NestedTestClass.new
+      subject.message = "foo"
+      expect(subject.valid?).to be_truthy
+      expect(subject.validation_errors).to be_empty
+    end
+
   end
 
-  class AttributeSupportTestClass
-    include Sequent::Core::Helpers::AttributeSupport, ActiveModel::Validations
-    attrs message: String, nested_test_class: NestedTestClass
-    validates_presence_of :message
-    validates_with Sequent::Core::Helpers::AssociationValidator, associations: :nested_test_class
-  end
+  context "including and inheritance" do
+    module TestModule
+      include Sequent::Core::Helpers::AttributeSupport
 
-  class SubTestClass < NestedTestClass
-    attrs sub_message: String
-    validates_presence_of :sub_message
-  end
+      attrs some_included_attribute: String
+    end
 
-  module TestModule
-    include Sequent::Core::Helpers::AttributeSupport
+    class TestClassWithIncludedModule
+      include Sequent::Core::Helpers::AttributeSupport, TestModule
 
-    attrs some_included_attribute: String
-  end
+      attrs some_attribute: String
+    end
 
-  class TestClassWithIncludedModule
-    include Sequent::Core::Helpers::AttributeSupport, TestModule
+    it "supports including modules" do
+      expect(TestClassWithIncludedModule.types).to eq({some_attribute: String, some_included_attribute: String})
+    end
 
-    attrs some_attribute: String
-  end
-
-  it "returns validation errors as hash" do
-    subject = NestedTestClass.new
-    expect(subject.valid?).to be_falsey
-    expect(subject.validation_errors).to have(1).items
-    expect(subject.validation_errors[:message]).to_not be_nil
-  end
-  
-  it "returns all validations of nested classes in same hash" do
-    subject = AttributeSupportTestClass.new
-    subject.nested_test_class = NestedTestClass.new
-    expect(subject.valid?).to be_falsey
-    expect(subject.validation_errors).to have(3).items
-    expect(subject.validation_errors[:message]).to_not be_nil
-    expect(subject.validation_errors[:nested_test_class]).to_not be_nil
-    expect(subject.validation_errors[:nested_test_class_message]).to_not be_nil
-  end
-
-  it "returns all validation errors including from superclass" do
-    subject = SubTestClass.new
-    expect(subject.valid?).to be_falsey
-    expect(subject.validation_errors).to have(2).items
-    expect(subject.validation_errors[:message]).to_not be_nil
-    expect(subject.validation_errors[:sub_message]).to_not be_nil
-  end
-
-  it "does not fail when it does not have errors" do
-    subject = NestedTestClass.new
-    subject.message = "foo"
-    expect(subject.valid?).to be_truthy
-    expect(subject.validation_errors).to be_empty
-  end
-
-  it "supports including modules" do
-    expect(TestClassWithIncludedModule.types).to eq({some_attribute: String, some_included_attribute: String})
-  end
-
-  it "should support subclasses" do
-    expect(SubTestClass.types).to eq({sub_message: String, message: String})
+    it "should support subclasses" do
+      expect(SubTestClass.types).to eq({sub_message: String, message: String})
+    end
   end
 
 end
