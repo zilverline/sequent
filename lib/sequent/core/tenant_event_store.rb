798bc60fefbaae58999cb987a2da61fc4432d540
Erik Rozendaal <erik@deler.org>
2015-05-15 15:50:11 +0200

    Optimized `replay_events_for(organization_id)` query

--- lib/sequent/core/tenant_event_store.rb
+++ lib/sequent/core/tenant_event_store.rb
@@ -8,11 +8,12 @@ module Sequent
         replay_events do
           event_record_class.connection.select_all(%Q{
 SELECT events.event_type, events.event_json
-  FROM #{quote_table_name event_record_class.table_name} aggregates
-       JOIN #{quote_table_name event_record_class.table_name} events ON aggregates.aggregate_id = events.aggregate_id
- WHERE aggregates.organization_id = #{quote organization_id}
-   AND aggregates.sequence_number = 1
-   AND aggregates.event_type <> #{quote snapshot_event_class.name}
+  FROM #{quote_table_name event_record_class.table_name} events
+ WHERE events.aggregate_id IN (SELECT aggregates.aggregate_id
+                                 FROM #{quote_table_name event_record_class.table_name} aggregates
+                                WHERE aggregates.organization_id = #{quote organization_id}
+                                  AND aggregates.sequence_number = 1
+                                  AND aggregates.event_type <> #{quote snapshot_event_class.name})
    AND events.event_type <> #{quote snapshot_event_class.name}
  ORDER BY events.id
 })
