49cd2b30010589bea36971e6e01e11f74151e5c3
Derek Kraan <derek.kraan@gmail.com>
2015-11-04 09:25:03 +0100

    Remove references to organization_id in schema.rb

--- db/sequent_schema.rb
+++ db/sequent_schema.rb
@@ -2,7 +2,6 @@ ActiveRecord::Schema.define do
 
   create_table "event_records", :force => true do |t|
     t.string "aggregate_id", :null => false
-    t.string "organization_id"
     t.integer "sequence_number", :null => false
     t.datetime "created_at", :null => false
     t.string "event_type", :null => false
@@ -12,7 +11,6 @@ ActiveRecord::Schema.define do
   end
 
   create_table "command_records", :force => true do |t|
-    t.string "organization_id"
     t.string "user_id"
     t.string "aggregate_id"
     t.string "command_type", :null => false
@@ -31,7 +29,6 @@ CREATE UNIQUE INDEX unique_event_per_aggregate ON event_records (
 CREATE INDEX snapshot_events ON event_records (aggregate_id, sequence_number DESC) WHERE event_type = 'Sequent::Core::SnapshotEvent'
 }
   add_index "event_records", ["command_record_id"], :name => "index_event_records_on_command_record_id"
-  add_index "event_records", ["organization_id"], :name => "index_event_records_on_organization_id"
   add_index "event_records", ["event_type"], :name => "index_event_records_on_event_type"
   add_index "event_records", ["created_at"], :name => "index_event_records_on_created_at"
 
