0d56443cba36c43999e21e780b2fb5845f576e4e
Derek Kraan <derek.kraan@gmail.com>
2017-05-22 09:08:08 +0200

    Delegate method `execute`

--- lib/sequent/core/base_event_handler.rb
+++ lib/sequent/core/base_event_handler.rb
@@ -44,7 +44,7 @@ module Sequent
 
       def_delegators :@record_session, :update_record, :create_record, :create_or_update_record, :get_record!, :get_record,
                      :delete_all_records, :update_all_records, :do_with_records, :do_with_record, :delete_record,
-                     :find_records, :last_record
+                     :find_records, :last_record, :execute
 
     end
 
