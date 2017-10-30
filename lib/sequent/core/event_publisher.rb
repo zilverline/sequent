1de7799143765e89edf084b1d9d082749350aba1
Derek Kraan <derek.kraan@gmail.com>
2017-10-30 15:00:33 +0100

    Synchronize events within a single thread

--- lib/sequent/core/event_publisher.rb
+++ lib/sequent/core/event_publisher.rb
@@ -1,6 +1,13 @@
 module Sequent
   module Core
     class EventPublisher
+      #
+      # EventPublisher ensures that, for every thread, events will be published in the order in which they are meant to be published.
+      #
+      # This potentially introduces a wrinkle into your plans: You therefore should not split a "unit of work" across multiple threads.
+      #
+      # If you do not want this, you are free to implement your own version of EventPublisher and configure sequent to use it.
+      #
       class PublishEventError < RuntimeError
         attr_reader :event_handler_class, :event
 
@@ -14,26 +21,34 @@ module Sequent
         end
       end
 
-      def initialize
-        @events_queue = Queue.new
-        @mutex = Mutex.new
-      end
-
       def publish_events(events)
         return if configuration.disable_event_handlers
-        events.each { |event| @events_queue.push(event) }
+        events.each { |event| events_queue.push(event) }
         process_events
       end
 
       private
 
-      def process_events
-        # only process events at the highest level
-        return if @mutex.locked?
+      def events_queue
+        Thread.current[:events_queue] ||= Queue.new
+      end
 
-        @mutex.synchronize do
-          while(!@events_queue.empty?) do
-            event = @events_queue.pop
+      def skip_if_locked(&block)
+        Thread.current[:events_queue_locked] = false if Thread.current[:events_queue_locked].nil?
+
+        return if Thread.current[:events_queue_locked]
+
+        Thread.current[:events_queue_locked] = true
+
+        block.yield
+      ensure
+        Thread.current[:events_queue_locked] = false
+      end
+
+      def process_events
+        skip_if_locked do
+          while(!events_queue.empty?) do
+            event = events_queue.pop
             configuration.event_handlers.each do |handler|
               begin
                 handler.handle_message event
