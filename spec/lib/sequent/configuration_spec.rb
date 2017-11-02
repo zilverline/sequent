4346a119e0ef5bc00847a6b5c2b95029a335c999
Derek Kraan <derek.kraan@gmail.com>
2017-11-02 14:24:22 +0100

    Fix specs that still relied on local config

--- spec/lib/sequent/configuration_spec.rb
+++ spec/lib/sequent/configuration_spec.rb
@@ -3,12 +3,6 @@ require 'spec_helper'
 describe Sequent::Configuration do
   let(:instance) { described_class.instance }
 
-  it 'configures a new aggregate store if the event store changes' do
-    new_event_store = double
-    Sequent.configuration.event_store = new_event_store
-    expect(Sequent.configuration.aggregate_repository.instance_variable_get(:@event_store)).to eq new_event_store
-  end
-
   context Sequent::Core::BaseCommandHandler do
     class SpecHandler < Sequent::Core::BaseCommandHandler
       def repository
