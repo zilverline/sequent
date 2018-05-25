fe2276082b7aa202d10f81795c1d65cced0cac7c
Lars Vonk <lars.vonk@gmail.com>
2018-05-25 13:35:08 +0200

    Dropped support for AR < 5

--- spec/lib/sequent/migration_class.rb
+++ spec/lib/sequent/migration_class.rb
@@ -1,5 +1,1 @@
-if ActiveRecord::VERSION::MAJOR <= 4
-  MigrationClass = ActiveRecord::Migration
-else
-  MigrationClass = ActiveRecord::Migration['4.2']
-end
+MigrationClass = ActiveRecord::Migration['4.2']
