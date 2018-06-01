eb26e466ea972bf8e3f760aaab6e83dead0a740e
Lars Vonk <lars.vonk@gmail.com>
2018-06-01 20:04:19 +0200

    Make db directory configurable

--- lib/sequent/configuration.rb
+++ lib/sequent/configuration.rb
@@ -12,8 +12,10 @@ module Sequent
     DEFAULT_REPLAYED_IDS_TABLE_NAME = 'sequent_replayed_ids'
 
     DEFAULT_MIGRATION_SQL_FILES_DIRECTORY = 'db/tables'
+    DEFAULT_DATABASE_CONFIG_DIRECTORY = 'db'
 
     DEFAULT_VIEW_SCHEMA_NAME = 'view_schema'
+    DEFAULT_EVENT_STORE_SCHEMA_NAME= 'sequent_schema'
 
     MIGRATIONS_CLASS_NAME = 'Sequent::Migrations::Projectors'
 
@@ -47,7 +49,8 @@ module Sequent
                   :view_schema_name,
                   :offline_replay_persistor_class,
                   :online_replay_persistor_class,
-                  :number_of_replay_processes
+                  :number_of_replay_processes,
+                  :database_config_directory,
 
     attr_reader :migrations_class_name,
                 :versions_table_name,
@@ -89,6 +92,7 @@ module Sequent
 
       self.offline_replay_persistor_class = DEFAULT_OFFLINE_REPLAY_PERSISTOR_CLASS
       self.online_replay_persistor_class = DEFAULT_ONLINE_REPLAY_PERSISTOR_CLASS
+      self.database_config_directory = DEFAULT_DATABASE_CONFIG_DIRECTORY
 
       self.logger = Logger.new(STDOUT).tap {|l| l.level = Logger::INFO }
     end
