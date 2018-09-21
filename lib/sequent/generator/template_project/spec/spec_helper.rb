3a2d25ca6bd37494bad3cdff0d98992bcad62190
Lars Vonk <lars.vonk@gmail.com>
2018-09-21 15:15:44 +0200

    Improve the generated spec_helper
    Ensure that lib spec do not have event_handlers. They are not
    needed to test the domain. Also ensure that the event_store
    is initialized before each spec. Otherwise events from previous
    specs will remain in the event_store. This breaks the `then_events`
    expectations.

--- lib/sequent/generator/template_project/spec/spec_helper.rb
+++ lib/sequent/generator/template_project/spec/spec_helper.rb
@@ -14,16 +14,35 @@ Sequent::Support::Database.establish_connection(db_config)
 Sequent::Support::Database.drop_schema!(Sequent.configuration.view_schema_name)
 
 Sequent::Migrations::ViewSchema.new(db_config: db_config).create_view_tables
-Sequent.configuration.event_store = Sequent::Test::CommandHandlerHelpers::FakeEventStore.new
+
+module DomainTests
+  def self.included(base)
+    base.metadata[:domain_tests] = true
+  end
+end
 
 RSpec.configure do |config|
   config.include Sequent::Test::CommandHandlerHelpers
+  config.include DomainTests, file_path: /spec\/lib/
+
+  # Domain tests run with a clean sequent configuration and the in memory FakeEventStore
+  config.around :each, :domain_tests do |example|
+    old_config = Sequent.configuration
+    Sequent::Configuration.reset
+    Sequent.configuration.event_store = Sequent::Test::CommandHandlerHelpers::FakeEventStore.new
+    Sequent.configuration.event_handlers = []
+    example.run
+  ensure
+    Sequent::Configuration.restore(old_config)
+  end
 
   config.around do |example|
     Sequent.configuration.aggregate_repository.clear
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.cleaning do
       example.run
+    ensure
+      Sequent.configuration.aggregate_repository.clear
     end
   end
 end
