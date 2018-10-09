c01fa1c7b8120bbda542203b55346e38b222ea54
Lars Vonk <lars.vonk@gmail.com>
2018-10-09 11:22:58 +0200

    Always validate Boolean type
    Like Date, DateTime and Integer the Boolean type should also
    always be validated.

---
+++ lib/sequent/core/helpers/boolean_validator.rb
@@ -0,0 +1,26 @@
+require 'active_model'
+require_relative 'value_validators'
+
+module Sequent
+  module Core
+    module Helpers
+      # Validates Boolean's
+      # Automatically included when using a
+      #
+      #   attrs value: Boolean
+      #
+      # The values:
+      #
+      # `true`, `false`, `'true'`, `'false'`, `nil`, and `blank?`
+      #
+      # are considered valid Booleans.
+      #
+      # They will be converted to `true`, `false` or `nil`
+      class BooleanValidator < ActiveModel::EachValidator
+        def validate_each(subject, attribute, value)
+          subject.errors.add attribute, :invalid_boolean unless Sequent::Core::Helpers::ValueValidators.for(Boolean).valid_value?(value)
+        end
+      end
+    end
+  end
+end
