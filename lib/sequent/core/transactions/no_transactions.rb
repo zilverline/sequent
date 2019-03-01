b2eeff0f7b31478b83994d3376c3ef1bdbb76e24
Lars Vonk <lars.vonk@gmail.com>
2019-03-01 11:38:26 +0100

    Add comment to clarify intent of NoTransactions

--- lib/sequent/core/transactions/no_transactions.rb
+++ lib/sequent/core/transactions/no_transactions.rb
@@ -1,7 +1,11 @@
 module Sequent
   module Core
     module Transactions
-
+      #
+      # NoTransactions is used when replaying the +ViewSchema+ for
+      # view schema upgrades. Transactions are not needed there since the
+      # view state will always be recreated anyway.
+      #
       class NoTransactions
         def transactional
           yield
