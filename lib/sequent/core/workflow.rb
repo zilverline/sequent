f079d6e14761617fca4e98dfef2952195a90c6ae
Sophie Déziel <courrier@sophiedeziel.com>
2019-03-04 19:26:42 -0500

    document the feature

--- lib/sequent/core/workflow.rb
+++ lib/sequent/core/workflow.rb
@@ -9,6 +9,13 @@ module Sequent
         Sequent.configuration.command_service.execute_commands(*commands)
       end
 
+      # Workflow#after_commit will accept a block to execute
+      # after the transaction commits. This is very useful to
+      # isolate side-effects. They will run only on the
+      # transaction's success and will not be able to roll it
+      # back when there is an exception. Useful if your background
+      # jobs processor is not using the same database connection
+      # to enqueue jobs.
       def after_commit(&block)
         Sequent.configuration.transaction_provider.after_commit &block
       end
