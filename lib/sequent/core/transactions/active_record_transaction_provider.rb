6480481a4d1bf364772c074eeb642cf0a9a9c617
Sophie Déziel <courrier@sophiedeziel.com>
2019-03-11 12:17:17 -0400

    replace usage of active-record's after_commits by our own callbacks queue

--- lib/sequent/core/transactions/active_record_transaction_provider.rb
+++ lib/sequent/core/transactions/active_record_transaction_provider.rb
@@ -7,12 +7,23 @@ module Sequent
           ActiveRecord::Base.transaction(requires_new: true) do
             yield
           end
+          after_commit_queue.each &:call
+        ensure
+          clear_after_commit_queue
         end
 
-        def after_commit
-          ActiveRecord::Base.after_commit do
-            yield
-          end
+        def after_commit(&block)
+          Thread.current[:after_commit_queue] << block
+        end
+
+        private
+
+        def after_commit_queue
+          Thread.current[:after_commit_queue] ||= []
+        end
+
+        def clear_after_commit_queue
+          Thread.current[:after_commit_queue] = []
         end
       end
 
