067e9a6154fe6aaa9b86e15edba058fa9684668c
Lars Vonk <lars.vonk@gmail.com>
2021-02-24 11:14:21 +0100

    Give `database_schema_directory` its own default
    And remove deprecation warning.

--- lib/notices.rb
+++ lib/notices.rb
@@ -2,15 +2,3 @@
 # in the logs during app boot. Adding such warnings in other places causes
 # lots of noise with duplicated messages, whereas this file is only
 # run once.
-
-warn <<~TEXT
-
-  [DEPRECATION] In future, the location of `sequent_migrations.rb` and
-  `sequent_schema.rb` will be determined by 
-  `config.database_schema_directory`. 
-
-  Add `config.database_schema_directory = 'config'` (or 
-  whatever location you use currently) to set the location in your 
-  configuration and avoid future errors.
-
-TEXT
