c05eff94d46676154ea62919039ce7779183a66e
Lars Vonk <lars.vonk@gmail.com>
2021-12-22 17:33:39 +0100

    Add some documentation why `requires_new` is set to `true`
    Taken from the git commit so it is easier to find.

--- lib/sequent/core/transactions/active_record_transaction_provider.rb
+++ lib/sequent/core/transactions/active_record_transaction_provider.rb
@@ -3,6 +3,31 @@
 module Sequent
   module Core
     module Transactions
+      ##
+      # Always require a new transaction.
+      #
+      # Read:
+      # http://api.rubyonrails.org/classes/ActiveRecord/Transactions/ClassMethods.html
+      #
+      # Without this change, there is a potential bug:
+      #
+      # ```ruby
+      # ActiveRecord::Base.transaction do
+      #   Sequent.configuration.command_service.execute_commands command
+      # end
+      #
+      # on Command do
+      #   do.some.things
+      #   fail ActiveRecord::Rollback
+      # end
+      # ```
+      #
+      # In this example, you might be surprised to find that `do.some.things`
+      # does not get rolled back! This is because AR doesn't automatically make
+      # a "savepoint" for us when we call `.transaction` in a nested manner. In
+      # order to enable this behaviour, we have to call `.transaction` like
+      # this: `.transaction(requires_new: true)`.
+      #
       class ActiveRecordTransactionProvider
         def transactional(&block)
           Sequent::ApplicationRecord.transaction(requires_new: true, &block)
