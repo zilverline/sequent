eed1c0a099045bd6b77f830ce3c2192acf1aa0f9
Boris Beginin <gen3212@gmail.com>
2022-04-14 15:00:46 +0400

    command_record.command producing wrong constant name exception (#315)
    Fix command_record.command producing wrong constant name exception

---
+++ spec/lib/sequent/core/command_record_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Sequent::Core::CommandRecord do
+  let(:command) { Sequent::Core::Command.new(aggregate_id: 'abc') }
+
+  subject { described_class.new }
+
+  describe '#command' do
+    before do
+      subject.command = command
+    end
+
+    it 'returns the original command' do
+      expect(subject.command).to eq command
+    end
+  end
+end
