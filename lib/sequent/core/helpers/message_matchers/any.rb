69d9f2e5016ff3290ab023bd9345ec089c719bad
Bob Forma <bobforma@gmail.com>
2022-05-10 10:48:30 +0200

    Support except opt in any

--- lib/sequent/core/helpers/message_matchers/any.rb
+++ lib/sequent/core/helpers/message_matchers/any.rb
@@ -4,13 +4,21 @@ module Sequent
   module Core
     module Helpers
       module MessageMatchers
-        class Any
-          def matches_message?(_message)
-            true
+        Any = Struct.new(:opts) do
+          include ExceptOpt
+
+          def matches_message?(message)
+            true unless excluded?(message)
           end
 
           def matcher_description
-            'any'
+            "any#{matcher_arguments}"
+          end
+
+          private
+
+          def matcher_arguments
+            "(except: #{except})" if except
           end
         end
       end
--- spec/lib/sequent/core/helpers/message_matchers/any_spec.rb
+++ spec/lib/sequent/core/helpers/message_matchers/any_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require_relative 'test_messages'
 
 describe Sequent::Core::Helpers::MessageMatchers::Any do
-  let(:matcher) { Sequent::Core::Helpers::MessageMatchers::Any.new }
+  let(:matcher) { Sequent::Core::Helpers::MessageMatchers::Any.new(**opts) }
 
   describe '#matches_message?' do
     subject { matcher.matches_message?(message) }
@@ -12,8 +12,52 @@ describe Sequent::Core::Helpers::MessageMatchers::Any do
     let(:message) { TestMessage.new(attrs) }
     let(:attrs) { {aggregate_id: 'x', sequence_number: 1} }
 
-    it 'returns true' do
-      expect(subject).to be_truthy
+    context 'given no opts' do
+      let(:opts) { {} }
+
+      it 'returns true' do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context 'given an except opt' do
+      let(:opts) { {except: except} }
+
+      context 'and it matches' do
+        let(:except) { TestMessage }
+
+        it 'returns false' do
+          expect(subject).to be_falsey
+        end
+      end
+
+      context 'and it does not match' do
+        let(:except) { OtherTestMessage }
+
+        it 'returns true' do
+          expect(subject).to be_truthy
+        end
+      end
+    end
+  end
+
+  describe '#matcher_description' do
+    subject { matcher.matcher_description }
+
+    context 'given no opts' do
+      let(:opts) { {} }
+
+      it 'only returns the matcher name' do
+        expect(subject).to eq('any')
+      end
+    end
+
+    context 'given an except opt' do
+      let(:opts) { {except: TestMessage} }
+
+      it 'returns the matcher name and except opt' do
+        expect(subject).to eq('any(except: TestMessage)')
+      end
     end
   end
 end
