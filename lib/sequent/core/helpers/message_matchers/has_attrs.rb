0a227aa3c8ee4dff92ca99f325d6ffe49daf77b4
Bob Forma <bobforma@gmail.com>
2022-05-10 09:42:57 +0200

    Support specifying message matcher in has_attrs

--- lib/sequent/core/helpers/message_matchers/has_attrs.rb
+++ lib/sequent/core/helpers/message_matchers/has_attrs.rb
@@ -4,14 +4,28 @@ module Sequent
   module Core
     module Helpers
       module MessageMatchers
-        HasAttrs = Struct.new(:expected_attrs) do
-          def initialize(expected_attrs)
+        HasAttrs = Struct.new(:message_matcher, :expected_attrs) do
+          def initialize(message_matcher, expected_attrs)
             super
 
+            fail ArgumentError, 'Missing required message matcher' if message_matcher.nil?
             fail ArgumentError, 'Missing required expected attrs' if expected_attrs.blank?
+
+            self.message_matcher = ArgumentCoercer.coerce_argument(message_matcher)
           end
 
           def matches_message?(message)
+            message_matcher.matches_message?(message) &&
+              matches_attrs?(message)
+          end
+
+          def matcher_description
+            "has_attrs(#{message_matcher.try(:matcher_description) || message_matcher.to_s}, #{matcher_arguments})"
+          end
+
+          private
+
+          def matches_attrs?(message)
             expected_attrs.all? do |(name, value)|
               if value.respond_to?(:matches_attr?)
                 value.matches_attr?(message.attributes[name])
@@ -20,12 +34,6 @@ module Sequent
               end
             end
           end
-
-          def matcher_description
-            "has_attrs(#{matcher_arguments})"
-          end
-
-          private
 
           def matcher_arguments
             expected_attrs
--- spec/lib/sequent/core/helpers/message_matchers/has_attrs_spec.rb
+++ spec/lib/sequent/core/helpers/message_matchers/has_attrs_spec.rb
@@ -4,59 +4,92 @@ require 'spec_helper'
 require_relative 'test_messages'
 
 describe Sequent::Core::Helpers::MessageMatchers::HasAttrs do
-  let(:matcher) { Sequent::Core::Helpers::MessageMatchers::HasAttrs.new(expected_attrs) }
+  let(:matcher) { Sequent::Core::Helpers::MessageMatchers::HasAttrs.new(message_matcher, expected_attrs) }
   let(:message) { TestMessage.new(attrs) }
   let(:attrs) { {aggregate_id: 'x', sequence_number: 1} }
+  let(:message_matcher) { TestMessage }
   let(:expected_attrs) { {aggregate_id: 'x', sequence_number: 1} }
 
   describe '#matches_message?' do
     subject { matcher.matches_message?(message) }
 
-    context 'given the message matches all of the expected attrs' do
-      it 'returns true' do
-        expect(subject).to be_truthy
-      end
+    context 'given the message matcher argument is a class' do
+      context 'and the class matches' do
+        let(:message_matcher) { TestMessage }
 
-      context 'and an expected value is a message matcher' do
-        let(:expected_attrs) do
-          {
-            aggregate_id: 'x',
-            sequence_number: Sequent::Core::Helpers::AttrMatchers::GreaterThanEquals.new(0),
-          }
+        context 'and the message matches all of the expected attrs' do
+          it 'returns true' do
+            expect(subject).to be_truthy
+          end
+
+          context 'and an expected value is a message matcher' do
+            let(:expected_attrs) do
+              {
+                aggregate_id: 'x',
+                sequence_number: Sequent::Core::Helpers::AttrMatchers::GreaterThanEquals.new(0),
+              }
+            end
+
+            it 'evaluates and returns true' do
+              expect(subject).to be_truthy
+            end
+          end
         end
 
-        it 'evaluates and returns true' do
-          expect(subject).to be_truthy
+        context 'and the message matches some of the expected attrs' do
+          let(:attrs) { {aggregate_id: 'x', sequence_number: 2} }
+
+          it 'returns false' do
+            expect(subject).to be_falsey
+          end
+
+          context 'and an expected value is a message matcher' do
+            let(:expected_attrs) do
+              {
+                aggregate_id: 'x',
+                sequence_number: Sequent::Core::Helpers::AttrMatchers::GreaterThanEquals.new(3),
+              }
+            end
+
+            it 'evaluates and returns false' do
+              expect(subject).to be_falsey
+            end
+          end
+        end
+
+        context 'and the message matches none of the expected attrs' do
+          let(:attrs) { {aggregate_id: 'y', sequence_number: 2} }
+
+          it 'returns false' do
+            expect(subject).to be_falsey
+          end
         end
       end
-    end
 
-    context 'given the message matches some of the expected attrs' do
-      let(:attrs) { {aggregate_id: 'x', sequence_number: 2} }
+      context 'and the class does not match' do
+        let(:message_matcher) { OtherTestMessage }
 
-      it 'returns false' do
-        expect(subject).to be_falsey
-      end
-
-      context 'and an expected value is a message matcher' do
-        let(:expected_attrs) do
-          {
-            aggregate_id: 'x',
-            sequence_number: Sequent::Core::Helpers::AttrMatchers::GreaterThanEquals.new(3),
-          }
-        end
-
-        it 'evaluates and returns false' do
+        it 'returns false' do
           expect(subject).to be_falsey
         end
       end
     end
 
-    context 'given the message matches none of the expected attrs' do
-      let(:attrs) { {aggregate_id: 'y', sequence_number: 2} }
+    context 'given the message matcher argument responds to matches_message?' do
+      context 'and the matcher matches' do
+        let(:message_matcher) { Sequent::Core::Helpers::MessageMatchers::IsA.new(TestModule) }
 
-      it 'returns false' do
-        expect(subject).to be_falsey
+        it 'returns true' do
+          expect(subject).to be_truthy
+        end
+      end
+
+      context 'and the matcher does not match' do
+        let(:message_matcher) { Sequent::Core::Helpers::MessageMatchers::IsA.new(TestModule, except: TestMessage) }
+
+        it 'returns false' do
+          expect(subject).to be_falsey
+        end
       end
     end
 
@@ -83,7 +116,7 @@ describe Sequent::Core::Helpers::MessageMatchers::HasAttrs do
     subject { matcher.matcher_description }
 
     it 'returns a description for the matcher including all expected attrs' do
-      expect(subject).to eq(%[has_attrs(aggregate_id: 'x', sequence_number: 1)])
+      expect(subject).to eq(%[has_attrs(TestMessage, aggregate_id: 'x', sequence_number: 1)])
     end
   end
 end
