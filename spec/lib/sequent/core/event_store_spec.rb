f9ce616c6fb63bb04dac6310c849ebe340b33b08
Bob Forma <bobforma@gmail.com>
2023-01-10 16:43:25 +0100

    Introduce separate test event class

--- spec/lib/sequent/core/event_store_spec.rb
+++ spec/lib/sequent/core/event_store_spec.rb
@@ -182,26 +182,28 @@ describe Sequent::Core::EventStore do
       let(:event_store) { Sequent::Core::EventStore.new(cache_event_types: false) }
 
       it 'returns the stream and events for existing aggregates' do
+        TestEventForCaching = Class.new(Sequent::Core::Event)
+
         event_store.commit_events(
           Sequent::Core::CommandRecord.new,
           [
             [
               Sequent::Core::EventStream.new(aggregate_type: 'MyAggregate', aggregate_id: aggregate_id),
-              [MyEvent.new(aggregate_id: aggregate_id, sequence_number: 1)],
+              [TestEventForCaching.new(aggregate_id: aggregate_id, sequence_number: 1)],
             ],
           ],
         )
         stream, events = event_store.load_events(aggregate_id)
         expect(stream).to be
-        expect(events).to eq([MyEvent.new(aggregate_id: aggregate_id, sequence_number: 1)])
+        expect(events).to eq([TestEventForCaching.new(aggregate_id: aggregate_id, sequence_number: 1)])
 
-        # redefine MyEvent class (ie. simulate Rails auto-loading)
-        OldMyEvent = MyEvent
-        MyEvent = Class.new(Sequent::Core::Event)
+        # redefine TestEventForCaching class (ie. simulate Rails auto-loading)
+        OldTestEventForCaching = TestEventForCaching
+        TestEventForCaching = Class.new(Sequent::Core::Event)
 
         stream, events = event_store.load_events(aggregate_id)
         expect(stream).to be
-        expect(events).to eq([MyEvent.new(aggregate_id: aggregate_id, sequence_number: 1)])
+        expect(events).to eq([TestEventForCaching.new(aggregate_id: aggregate_id, sequence_number: 1)])
       end
     end
   end
