984a1ca98009eee1a01d3972f43aacbe1ac2baca
Lars Vonk <lars.vonk@gmail.com>
2023-11-02 11:15:31 +0100

    Improve check for running in Rails
    For applications running outside Rails but with monkeypatched Rails module.

--- integration-specs/simple/spec/app_spec.rb
+++ integration-specs/simple/spec/app_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe 'app' do
-  context 'registering comand handlers' do
+  context 'registering command handlers' do
     let(:command_handler_classes) { Sequent.configuration.command_handlers.map(&:class).sort_by(&:name) }
 
     it 'autoregisters command handlers' do
@@ -25,6 +25,18 @@ describe 'app' do
         ManualWorkflow,
         SecondProjector,
       ]
+    end
+  end
+
+  context 'with a monkey patched Rails module' do
+    before do
+      module Rails; end
+    end
+
+    it 'can autoregister' do
+      Sequent.configure do |config|
+        config.enable_autoregistration = true
+      end
     end
   end
 end
