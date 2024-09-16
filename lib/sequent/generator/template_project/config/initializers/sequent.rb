70d8c24baddffb12ffd637c2ae4ea80a126381ac
Erik Rozendaal <erik@deler.org>
2024-09-16 17:30:24 +0200

    Rubocop fixes in template project

--- lib/sequent/generator/template_project/config/initializers/sequent.rb
+++ lib/sequent/generator/template_project/config/initializers/sequent.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require './db/migrations'
 
 Sequent.configure do |config|
   config.migrations_class_name = 'Migrations'
 
   config.command_handlers = [
-    PostCommandHandler.new
+    PostCommandHandler.new,
   ]
 
   config.event_handlers = [
-    PostProjector.new
+    PostProjector.new,
   ]
 end
