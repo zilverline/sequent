17f0def164a6776b9d6660adf9b4a531f21ddd99
Erik Rozendaal <erik@deler.org>
2025-08-21 16:34:21 +0200

    Only exclude view schema if managed by old migrations process

--- lib/sequent/rake/migration_tasks.rb
+++ lib/sequent/rake/migration_tasks.rb
@@ -106,10 +106,13 @@ module Sequent
                 begin
                   ActiveRecord.dump_schemas = nil
                   ActiveRecord::Tasks::DatabaseTasks.structure_dump_flags = %W[
-                    --exclude-schema=#{Sequent.configuration.view_schema_name}
                     --exclude-schema=#{Sequent.configuration.replay_schema_name}
                     --exclude-schema=#{Sequent.configuration.archive_schema_name}
                   ]
+                  if Sequent.configuration.migrations_class
+                    ActiveRecord::Tasks::DatabaseTasks.structure_dump_flags <<
+                      "--exclude-schema=#{Sequent.configuration.view_schema_name}"
+                  end
                   DatabaseTasks.dump_schema(db_config, :sql)
                 ensure
                   ActiveRecord.dump_schemas = old_dump_schemas
